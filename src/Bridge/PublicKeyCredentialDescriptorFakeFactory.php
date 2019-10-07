e60a50eb0be9d19157a023a1bd43c7440378516d
Marius Klocke <m.klocke@reply.de>
2019-10-07 12:44:26 +0200

    [TASK] Generate fakes by hashing instead of xor encryption

--- src/Bridge/PublicKeyCredentialDescriptorFakeFactory.php
+++ src/Bridge/PublicKeyCredentialDescriptorFakeFactory.php
@@ -2,39 +2,23 @@
 
 namespace Reply\WebAuthn\Bridge;
 
+use RuntimeException;
 use Webauthn\PublicKeyCredentialDescriptor;
 
 class PublicKeyCredentialDescriptorFakeFactory
 {
     public function create(string $username): PublicKeyCredentialDescriptor
     {
-        $secret = hex2bin(getenv('APP_SECRET'));
+        $salt = getenv('APP_SECRET');
+        if (!is_string($salt) || empty($salt)) {
+            throw new RuntimeException('Invalid value for environment variable APP_SECRET');
+        }
+
+        $hash = hash('sha512', $salt . $username);
 
         return new PublicKeyCredentialDescriptor(
             PublicKeyCredentialDescriptor::CREDENTIAL_TYPE_PUBLIC_KEY,
-            $this->xorCrypt($this->strFill($username, 64), $this->strFill($secret, 64))
+            hex2bin($hash)
         );
-    }
-
-    private function xorCrypt(string $data, string $key): string
-    {
-        if (strlen($data) !== strlen($key)) {
-            throw new \RuntimeException('XOR Encryption failed due to unequal string lengths');
-        }
-        $encrypted = '';
-        $n = strlen($data);
-        for ($i = 0; $i < $n; $i++) {
-            $encrypted .= $data[$i] ^ $key[$i];
-        }
-        return $encrypted;
-    }
-
-    private function strFill(string $string, int $targetLength): string
-    {
-        while (strlen($string) < $targetLength) {
-            $string .= $string;
-        }
-
-        return substr($string, 0, $targetLength);
     }
 }
