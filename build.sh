4bc15f61e3f6efe2f075d8ed8c2074e735fac8ab
Marius Klocke <m.klocke@reply.de>
2020-01-24 11:50:34 +0100

    [TASK] Refactor .gitlab-ci.yml

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+phpcs --standard=PSR2 --extensions=php --warning-severity=0 .
 rm .gitignore
 composer install --no-dev --no-interaction --no-suggest --ignore-platform-reqs
+phpunit --testdox
 composer archive --dir build/ --format=zip
