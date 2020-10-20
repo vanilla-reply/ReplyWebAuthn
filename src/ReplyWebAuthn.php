0c64b1fd861ef69978c4350e2c572f00d4086b51
Marius Klocke <m.klocke@reply.de>
2020-10-20 10:57:56 +0200

    [TASK] Move CREATE TABLE statements from Migration to main class

--- src/ReplyWebAuthn.php
+++ src/ReplyWebAuthn.php
@@ -27,6 +27,9 @@ class ReplyWebAuthn extends Plugin
         }
 
         parent::install($installContext);
+
+        $connection = $this->container->get(Connection::class);
+        $this->createTables($connection);
         $this->writeDefaultConfig();
     }
 
@@ -81,6 +84,34 @@ class ReplyWebAuthn extends Plugin
     private function dropTables(Connection $connection): void
     {
         $connection->executeQuery("DROP TABLE IF EXISTS `customer_credential`");
+    }
+
+    private function createTables(Connection $connection): void
+    {
+        $connection->executeQuery('
+            CREATE TABLE IF NOT EXISTS `customer_credential` (
+              `id` varbinary(255) NOT NULL PRIMARY KEY,
+              `user_handle` binary(16),
+              `name` varchar(255) NOT NULL,
+              `type` varchar(255) NOT NULL,
+              `transports` json NOT NULL,
+              `attestation_type` varchar(255) NOT NULL,
+              `trust_path` json NOT NULL,
+              `aaguid` varchar(255) NOT NULL,
+              `public_key` varbinary(255) NOT NULL,
+              `counter` int(11) NOT NULL,
+              `created_at` datetime(3) NOT NULL,
+              `updated_at` datetime(3),
+              CONSTRAINT `fk.customer_credential.user_handle` FOREIGN KEY (`user_handle`)
+                REFERENCES `customer` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
+              CONSTRAINT `uniq.customer_credential.public_key`
+                UNIQUE (`public_key`),
+              CONSTRAINT `json.customer_credential.transports`
+                CHECK (JSON_VALID(`transports`)),
+              CONSTRAINT `json.customer_credential.trust_path`
+                CHECK (JSON_VALID(`trust_path`))
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+        ');
     }
 
     /**
