3987a51f8ecbd48968d6488a2b4269ba8b6be388
Marius Klocke <m.klocke@reply.de>
2020-10-21 11:07:12 +0200

    [TASK] Move JS code for login to index.html.twig

--- src/Resources/app/administration/src/main.js
+++
@@ -1,9 +0,0 @@
-import template from './extension/sw-login-login.html.twig';
-import deDE from './snippet/de-DE.json';
-import enGB from './snippet/en-GB.json';
-
-Shopware.Locale.extend('de-DE', deDE);
-Shopware.Locale.extend('en-GB', enGB);
-Shopware.Component.override('sw-login-login', {
-    template
-});
